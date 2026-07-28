9182cc8a4336d32a557a563ec6e6b0008b5c760f
adam <adam.menq@icloud.com>
2026-07-28 12:47:29 -0600

    dependencies and todos

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ server = FastAPI()
 @server.get("/")
 async def get_root():
     return {"test"}
+
+
+# Db connection
+ 
+# Login Logic
+
+# Create task (Dockerfile generation)
+
+# Run task (Dockerfile execution and attaching to the container logs or continously streaming log files to the frontend)
+
