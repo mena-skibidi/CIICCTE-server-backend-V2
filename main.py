28ca20a4658d657b530d34308ac855b4a32c986b
Adam Mena <adam.menq@icloud.com>
2026-08-02 07:24:56 -0600

    db connection, dependencies fix

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
+from sqlmodel import create_engine, Session, Field, select, SQLModel
 
 server = FastAPI()
 
+# Habria que esconder la connection string en un .env o en las variables de entorno
+engine = create_engine("postgresql://dbuser:labtest321@db:5432/labdb", echo=True)
+
+class Test(SQLModel, table = True):
+    id: int = Field(primary_key=True)
+    text: str
+
+
 @server.get("/")
 async def get_root():
-    return {"test"}
+    with Session(engine) as session:
+        statement = select(Test)
+        results = session.exec(statement).all()
+        return results
 
 
 '''
 TODO
 
-- Modelar tablas y relaciones
-- Conexion a la db
-- Hacer la logica del login
+x Modelar tablas y relaciones
+o Conexion a la db
+x Hacer la logica del login
 
 '''
 
