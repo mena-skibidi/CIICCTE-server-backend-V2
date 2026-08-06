cb2a36dd27467ad1a02315c5bc7723ef35268e5b
Adam Mena <adamskibidi616@gmail.com>
2026-08-05 23:23:42 -0600

    login tables

--- main.py
+++ main.py
@@ -1,32 +1,49 @@
 from fastapi import FastAPI
-from sqlmodel import create_engine, Session, Field, select, SQLModel
+from sqlmodel import create_engine, Field, SQLModel
 
 server = FastAPI()
 
-# Habria que esconder la connection string en un .env o en las variables de entorno
+
+class roles(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    nombre_rol: str
+
+
+class usuarios(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    username: str
+    nombre_completo: str
+    password_encriptada: str
+
+
+class cuenta(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    usuarios_id: int = Field(default=None, foreign_key="usuarios.id")
+    roles_id: int = Field(default=None, foreign_key="roles.id")
+
+
 engine = create_engine("postgresql://dbuser:labtest321@db:5432/labdb", echo=True)
 
-class Test(SQLModel, table = True):
-    id: int = Field(primary_key=True)
-    text: str
+
+def create_tables():
+    SQLModel.metadata.create_all(engine)
 
 
 @server.get("/")
 async def get_root():
-    with Session(engine) as session:
-        statement = select(Test)
-        results = session.exec(statement).all()
-        return results
+    return "test"
 
 
-'''
+@server.post("/roles")
+async def create_role():
+    return
+
+
+"""
 TODO
 
 x Modelar tablas y relaciones
 o Conexion a la db
 x Hacer la logica del login
 
-'''
-
-
-
+"""
